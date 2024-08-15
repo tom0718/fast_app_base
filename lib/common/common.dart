10a888d871913faee64ff55a35a568a998b2575f
tommy <tom0718@naver.com>
2024-08-15 10:53:03 +0900

    Update common.dart
    add file

--- lib/common/common.dart
+++ lib/common/common.dart
@@ -12,6 +12,7 @@ export '../common/dart/extension/animation_controller_extension.dart';
 export '../common/dart/extension/collection_extension.dart';
 export '../common/dart/extension/context_extension.dart';
 export '../common/dart/extension/num_extension.dart';
+export '../common/dart/extension/num_duration_extension.dart';
 export '../common/dart/extension/velocityx_extension.dart';
 export '../common/dart/kotlin_style/kotlin_extension.dart';
 export 'constants.dart';
