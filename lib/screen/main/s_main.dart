bdfcdb58f36b4ccc3477e0252bc6bbbc3f3d7b7f
tommy <tom0718@naver.com>
2024-08-15 11:54:46 +0900

    Update s_main.dart
    splash screen

--- lib/screen/main/s_main.dart
+++ lib/screen/main/s_main.dart
@@ -1,6 +1,7 @@
 import 'package:fast_app_base/screen/main/tab/tab_item.dart';
 import 'package:fast_app_base/screen/main/tab/tab_navigator.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
 
 import '../../common/common.dart';
 import 'w_menu_drawer.dart';
@@ -12,7 +13,7 @@ class MainScreen extends StatefulWidget {
   State<MainScreen> createState() => MainScreenState();
 }
 
-class MainScreenState extends State<MainScreen> with SingleTickerProviderStateMixin {
+class MainScreenState extends State<MainScreen> with SingleTickerProviderStateMixin, AfterLayoutMixin {
   TabItem _currentTab = TabItem.home;
   final tabs = [TabItem.home, TabItem.favorite];
   final List<GlobalKey<NavigatorState>> navigatorKeys = [];
@@ -25,6 +26,17 @@ class MainScreenState extends State<MainScreen> with SingleTickerProviderStateMi
 
   static double get bottomNavigationBarBorderRadius => 30.0;
 
+  @override
+  FutureOr<void> afterFirstLayout(BuildContext context) async {
+
+    // await login();
+    // await loading();
+
+    delay(() {
+      FlutterNativeSplash.remove();
+    }, 1500.ms);
+  }
+  
   @override
   void initState() {
     super.initState();
